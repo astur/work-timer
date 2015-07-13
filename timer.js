c9271a17b49b9967a1517e0cf23a0a0714f48b56
astur <astur@yandex.ru>
2015-07-14 00:38:17 +0300

    StoredObj in lib/so.js

--- timer.js
+++ timer.js
@@ -188,23 +188,4 @@ function displaySavedPeriods() {
     });
 }
 
-function StoredObj(sn, defs){
-    var storageName = sn === undefined ? 'myStorage' : sn;
-    this.save = function(){
-        localStorage[storageName] = JSON.stringify(this);
-    }
-    if (localStorage.getItem(storageName) !== null) {
-        var inObj = JSON.parse(localStorage[storageName]);
-        for(var key in inObj){
-            this[key] = inObj[key];
-        }
-    }
-    if (typeof defs === 'object') {
-        for(var key in defs){
-            this[key] = this[key] === undefined ? defs[key] : this[key];
-        }
-        this.save();
-    };
-}
-
 });
